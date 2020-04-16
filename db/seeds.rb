2f68b2ef53b55e73cef11f85a7fc038445b14d8a
Bryant Richards <dragonseyeimages@gmail.com>
2020-04-15 22:15:07 -0500

    set up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "Bob", email: "bob@test.com", password: "test1234")
+# User.create(name: "Bob", email: "bob@test.com", password: "test1234")
+# Dvd.create(title: "Goonies", year: 1985, description: "Old-fashioned yarn about a band of adventurous kids who take on the might of a property developing company which plans to destroy their home to build a country club. When the children discover an old pirate map in the attic, they follow it into an underground cavern in search of lost treasure but come up against plenty of dangerous obstacles along the way.", user_id: 1)
+# Book.create(title: "Towers of Sunset", author: "L. E. Modesitt Jr.", description: "Rather than accepting a marriage arranged by his mother, the powerful military matriarch of Westwind, Creslin chooses exile, setting out to find his own identity and developing his magical talents through conflict with the enigmatic white wizards of Candar. What Creslin doesn't know he stands in the way of their plot to subjugate the world.", user_id: 1)
