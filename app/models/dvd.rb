f684f326658b13afd898bafcb9d43c1e574c04e8
Bryant Richards <dragonseyeimages@gmail.com>
2020-04-19 00:53:44 -0500

    added validations

--- app/models/dvd.rb
+++ app/models/dvd.rb
@@ -1,4 +1,6 @@
 class Dvd < ApplicationRecord
 
   belongs_to :user
+
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
   has_many :books
   has_many :dvds
+
+  validates :name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, length: { in: 6..20 }
 end
